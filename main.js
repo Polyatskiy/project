c525f8f8c578f8b79e8ff56b574ff062412692c4
Valeriy <polyatskiy@gmail.com>
2021-01-30 21:27:34 +0200

    petrichenko lesson 12

--- main.js
+++ main.js
@@ -74,20 +74,42 @@
 // let incr = 10,
 //     decr = 10;
 
-    // ++incr;
-    // --decr;
+// ++incr;
+// --decr;
 
-    // console.log(++incr);
-    // console.log(--decr);
-    
-    
-    console.log(5%2);
-    console.log(2*4 === '8');
+// console.log(++incr);
+// console.log(--decr);
 
-    console.log(2 + 2 * 2 === 8);
 
-    const isChecked = true,
-            isClose = false;
+// console.log(5%2);
+// console.log(2*4 === '8');
 
-            console.log(isChecked && isClose);
-            console.log(isChecked || isClose);
+// console.log(2 + 2 * 2 === 8);
+
+// const isChecked = true,
+//         isClose = false;
+
+//         console.log(isChecked && isClose);
+//         console.log(isChecked || isClose);
+
+
+// Lesson 12
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
