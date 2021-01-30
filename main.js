a08adb004146a1fc2c9a4bde613cd8868dd65374
Valeriy <polyatskiy@gmail.com>
2021-01-30 08:50:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// let number = 5;
+// const leftBorderWidth =1;
+
+// number = 10;
+// console.log(number); 
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// Lesson 5
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+
+// const bool = true;
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// // console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+
+// Lesson 7
+// alert('Hello');
+
+// const result = confirm('Are you here');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '')
+// answers[1] = prompt('Как ваша Фамилия?', '')
+// answers[2] = prompt('Сколько вам лет?', '')
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// Lesson 8
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+// Lesson 9
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+    // ++incr;
+    // --decr;
+
+    // console.log(++incr);
+    // console.log(--decr);
+    
+    
+    console.log(5%2);
+    console.log(2*4 === '8');
+
+    console.log(2 + 2 * 2 === 8);
+
+    const isChecked = true,
+            isClose = false;
+
+            console.log(isChecked && isClose);
+            console.log(isChecked || isClose);
